450148720e5f988c6ada99082ea48540b0f562e6
titi_ <titi867>
2023-06-07 10:25:28 +0200

    eliminados: el primer comentario, los comentarios sobre el autor y se ordenó el formato

--- src/prácticaunidad6/Main.java
+++ src/prácticaunidad6/Main.java
@@ -1,42 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package prácticaunidad6;
 
-package prácticaunidad3;
-
-/**
- *
- * @author ED05
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         CCuenta miCuenta;
         double saldoActual;
-        
-        miCuenta = new CCuenta("Manuel Mantecas","0000-1234-55-123456789",3000,0);
-        try 
-        {
+
+        miCuenta = new CCuenta("Manuel Mantecas", "0000-1234-55-123456789", 3000, 0);
+        try {
             miCuenta.retirar(2300);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
-        
-        try
-        {
+
+        try {
             System.out.println("Ingreso en cuenta");
             miCuenta.ingresar(695);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
         saldoActual = miCuenta.getEstado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es" + saldoActual);
     }
 
 }
