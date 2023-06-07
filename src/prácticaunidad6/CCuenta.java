7993eb3b63cb69a056c50c801cc80a4b6165c2f4
titi_ <titi867>
2023-06-07 10:24:42 +0200

    eliminados: el primer comentario, los comentarios sobre el autor y se ordenó el formato. Se dejó la variable tipoInteres como comentario ya que no se ha usado

--- src/prácticaunidad6/CCuenta.java
+++ src/prácticaunidad6/CCuenta.java
@@ -1,78 +1,64 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package prácticaunidad6;
 
-package prácticaunidad3;
-
-/**
- *
- * @author ED05
- */
 public class CCuenta {
-
 
     // Propiedades de la Clase Cuenta
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    //private double tipoInteres;
 
     /* Constructor sin argumentos */
-    public CCuenta ()
-    {
+    public CCuenta() {
     }
+
     // Constructor con parámetro para iniciar todas las propiedades de la clase
-    public CCuenta (String nom, String cue, double sal, double tipo)
-    {
-        nombre=nom;
-        cuenta=cue;
-        saldo=sal;
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
-   // Método para asignar el nombre del titular de la cuenta
-    public void setNombre(String nom)
-    {
-        nombre=nom;
+    // Método para asignar el nombre del titular de la cuenta
+
+    public void setNombre(String nom) {
+        nombre = nom;
     }
+
     // Método que me devuelve el nombre del titular
-    public String getNombre()
-    {
+    public String getNombre() {
         return nombre;
     }
 
     // Método que me devuelve el saldo disponible en cada momento
-     public double getEstado ()
-    {
+    public double getEstado() {
         return saldo;
     }
 
     /* Método para ingresar cantidades en la cuenta. Modifica el saldo.
      * Este método va a ser probado con Junit
      */
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         saldo = saldo + cantidad;
     }
-
-
 
     /* Método para retirar cantidades en la cuenta. Modifica el saldo.
      * Este método va a ser probado con Junit
      */
-    public void retirar (double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (getEstado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (getEstado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         saldo = saldo - cantidad;
     }
 
     // Método que me devuelve el número de cuenta
-    public String getCuenta ()
-    {
+    public String getCuenta() {
         return cuenta;
     }
 }
